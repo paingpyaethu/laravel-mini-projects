115530bece87363c27cbbd98e17a5f156ae288ea
Acer <paingpyaethu@gmail.com>
2022-01-18 01:46:39 +0630

    a

---
+++ echo.php
@@ -0,0 +1,9 @@
+<?php
+//    echo ('Paing Pyae Thu');
+    echo "<b>Paing Paing</b>";
+    echo "<h1><i>Paing Paing</i></h1>";
+    echo "Paing"."Paing";
+    echo "Paing","Paing";
+    echo 'Paing','Paing';
+    echo 'Paing'.'Paing';
+    echo 15646;
